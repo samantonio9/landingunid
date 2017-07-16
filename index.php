4c62979241a95788da8924b8c03da65e78ff9366
samantonio9 <zammis@socialastronauts.com>
2017-07-16 16:52:10 -0500

    s

--- index.php
+++ index.php
@@ -7,17 +7,23 @@
     <meta name="description" content="is an agency that takes you and your friends to the best clubs in Cancun on their featured nights. We ensure that the clubs we take you to will be packed with">
     <meta name="keywords" content="bachelor party cancun,party in cancun,cancun party,cocobongo,cocobongo cancun,party, cancun, beach, amazing, travel, vacation, enjoy, pleasure, bitch, springbreak, spring, cancun, coco bongo, mandala, the city, sr. frogs, dadyo, palazzo, elevate, pirate, dinner, chichen, xplor, xelha, xcaret, dolphin, shark, encounter, sea lion, excursion, tour, tours,">
     <meta name="author" content="@SocialAstronauts">
-    <title>{{ title }} | Party Rockers Cancun</title>
+    <title>Oferta Educativa UNID</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
-
+    <link rel="apple-touch-icon-precomposed" href="images/favicon_app.png">
+    <link rel="shortcut icon" type="image/png" href="images/favicon.png">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/Univers/Univers.otf">
+    <link rel="stylesheet" href="fonts/Univers/Univers-Light.otf">
+    <link rel="stylesheet" href="fonts/Univers/Univers-BoldExt.otf">
+    <link rel="stylesheet" href="fonts/Univers/Univers-BoldExt.otf">
 </head>
 <body>
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
             <div class="navbar-header">
-                <a class="navbar-brand" href="#">
+                <a class="navbar-brand" href="http://www.unid.edu.mx/cancun.html">
                     <img src="images/logotipo.png" width="180" alt="{{ op.logo.alt }}">
                 </a>
             </div>
@@ -30,15 +36,15 @@
 
     <section id="home" class="d-flex justify-content-center align-items-center">
         <div class="container principal">
-            <a href="#" class="btn-1">¡Inscripciones Abiertas!</a>
+            <a href="#inicio" class="btn-1">Más Información</a>
         </div>
     </section>
     <section id="ban-edu" class="d-flex justify-content-center align-items-center">
         <div class="container">
             <div class="col-6 ">
-                <h1>Maestría en <br>
+                <h2><span>Maestría en</span> <br>
                     Educación
-                </h1>
+                </h2>
                 <p class="py-5">
                     Será un especialista con una concepción y fundamentos innovadores y habilidades para aplicar y resolver problemas en el campo de la educación.
 
@@ -52,10 +58,10 @@
     <section id="ban-admin" class="d-flex justify-content-center align-items-center">
         <div class="container">
             <div class="col-6 offset-md-6 pl-5">
-                <h1>Maestría en <br>
+                <h2><span>Maestría en</span> <br>
                     Administración de <br>
                     negocios
-                </h1>
+                </h2>
                 <p class="py-5">
                     El enfoque es encaminar la empresa o institución  hacia los objetivos y metas a través de la eficiencia, ejerciendo un liderazgo claro en el equipo de trabajo,  la especialización es en: Dirección de Negocios.
                 </p>
@@ -67,10 +73,10 @@
     <section id="ban-merca" class="d-flex justify-content-center align-items-center">
         <div class="container">
             <div class="col-6">
-                <h1>Maestría en <br>
+                <h2><span>Maestría en</span> <br>
                     Mercadotecnia y <br>
                     medios digitales
-                </h1>
+                </h2>
                 <p class="py-5">
                     Contarás con conocimientos especializados en el análisis, aplicación y evaluación de proyectos asociados a la mercadotecnia y a la publicidad, para el desarrollo de productos y servicios, a través de medios digitales.
                 </p>
@@ -79,25 +85,11 @@
 
         </div>
     </section>
-    <section id="ban-dere" class="d-flex justify-content-center align-items-center">
-        <div class="container">
-            <div class="col-6 offset-md-6 pl-5">
-                <h1>Maestría en <br>
-                    Derecho Penal
-                </h1>
-                <p class="py-5">
-                    El enfoque es encaminar la empresa o institución  hacia los objetivos y metas a través de la eficiencia, ejerciendo un liderazgo claro en el equipo de trabajo,  la especialización es en: Dirección de Negocios.
-                </p>
-                <a href="">Ver Oferta completa</a>
-            </div>
-
-        </div>
-    </section>
-    <div id="inicio" class="d-flex justify-content-center align-items-cente">
+    <div id="inicio" class="d-flex justify-content-center align-items-cente py-5">
         <div class="container">
                 <div class="col">
-                    <h1>¡Iniciamos en septiembre!</h1>
-                    <p>Duración de 1 año 4 meses de 8:00 a 14:00 horas
+                    <h2>¡Iniciamos en septiembre!</h2>
+                    <p>Duración <span>de 1 año 4 meses</span> de 8:00 a 14:00 horas
                     </p>
                 </div>
         </div>
@@ -105,7 +97,7 @@
     <section id="formulario" class="d-flex justify-content-center align-items-cente">
         <div class="container">
             <div class="row">
-                <div class="col-6">
+                <div class="col-6 text-center">
                     <h2>
                         INICIA TU SOLICITUD
                         <br>
@@ -113,17 +105,115 @@
                     </h2>
 
                 </div>
-
-                <!-- www.123contactform.com script begins here -->
-                <script type="text/javascript" defer src="//www.123contactform.com/embed/2826482.js" data-role="form" data-default-width="650px"></script>
-                <p>Powered by <a class="footerLink13" title="123ContactForm" href="http://www.123contactform.com">123ContactForm</a> | <a style="font-size:small!important;color:#000000!important; text-decoration:underline!important;" title="Looks like phishing? Report it!" href="http://www.123contactform.com/sfnew.php?s=123contactform-52&control119314=http:///contact-form--2826482.html&control190=Report%20abuse" rel="nofollow">Report abuse</a></p><!-- www.123contactform.com script ends here -->
+                <div class="col-lg-6 col-md-12 contact container-1 shadow" id="contact_form">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 contact header py-4">
+                                <p>Completa el formulario para recibir mas información y atención personalizada</p>
+                                <div id="contact_results"></div>
+                            </div>
+                        </div>
+                        <div class="contact row body" id="">
+                            <div class="contact-elements">
+                                <div class="col-12 py-4">
+                                    <input type="text" name="name" id="name" required="true" class="input-field" placeholder="Nombre Completo" />
+                                </div>
+                                <div class="col py-4">
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <input type="email" name="email" required="true" class="input-field" placeholder="Correo Electronico" />
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="row">
+                                                <div class="col-4">
+                                                    <input type="text" name="phone2" maxlength="15" class="tel-number-field long" placeholder="Lada" />
+                                                </div>
+                                                <div class="col-8">
+                                                    <input type="text" name="phone2" maxlength="15" class="tel-number-field long" placeholder="Numero de telefono" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <label for="busqueda-en-google">
+                                        Selecciona un programa de tu interes
+                                    </label>
+                                    <div class="select">
+                                        <select name="busqueda-en-google" id="busqueda-en-google">
+                                            <option value="educacion">Maestría en Educación</option>
+                                            <option value="administracion">Maestría en Administración de negocios</option>
+                                            <option value="mercadotecnia">Maestría en Mercadotecnia y medios digitales</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col p-4">
+                                    <textarea cols="46" rows="8" name="message" id="message" class="textarea-field" required="true" placeholder="Comentarios"></textarea>
+                                </div>
+                                <div class="center-btn col d-flex justify-content-center">
+                                    <input id="submit_btn" class="btn btn-submit" type="submit" value="Enviar"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
-
-
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("#submit_btn").click(function() {
+
+                var proceed = true;
+                //simple validation at client's end
+                //loop through each field and we simply change border color to red for invalid fields
+                $("#contact_form input[required=true], #contact_form textarea[required=true]").each(function() {
+                    $(this).css('border-color', '');
+                    if (!$.trim($(this).val())) { //if this field is empty
+                        $(this).css('border-color', 'red'); //change border color to red
+                        proceed = false; //set do not proceed flag
+                    }
+                    //check invalid email
+                    var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+                    if ($(this).attr("type") == "email" && !email_reg.test($.trim($(this).val()))) {
+                        $(this).css('border-color', 'red'); //change border color to red
+                        proceed = false; //set do not proceed flag
+                    }
+                });
+
+                if (proceed) //everything looks good! proceed...
+                {
+                    //get input field values data to be sent to server
+                    post_data = {
+                        'user_name': $('input[name=name]').val(),
+                        'user_email': $('input[name=email]').val(),
+                        'phone_number': $('input[name=phone2]').val(),
+                        'msg': $('textarea[name=message]').val()
+                    };
+
+                    //Ajax post data to server
+                    $.post('php/contact_me.php', post_data, function(response) {
+                        if (response.type == 'error') { //load json data from server and output message
+                            output = '<div class="error">' + response.text + '</div>';
+                        } else {
+                            output = '<div class="success">' + response.text + '</div>';
+                            //reset values in all input fields
+                            $("#contact_form  input[required=true], #contact_form textarea[required=true]").val('');
+                            $("#contact_form #contact_body").slideUp(); //hide form after success
+                        }
+                        $("#contact_form #contact_results").hide().html(output).slideDown();
+                    }, 'json');
+                }
+            });
+
+            //reset previously set border colors and hide all message on .keyup()
+            $("#contact_form  input[required=true], #contact_form textarea[required=true]").keyup(function() {
+                $(this).css('border-color', '');
+                $("#result").slideUp();
+            });
+        });
+    </script>
 <script src="js/main.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 </body>
